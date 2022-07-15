568f1deee02c5cf2ccf2f2ce341d67e8fb6be203
Daniel Custodio <play.daniel@hotmail.com>
2022-07-15 17:36:45 -0300

    feat: validação de usuário existente no método PUT de user

--- src/routers/userRoutes.js
+++ src/routers/userRoutes.js
@@ -2,11 +2,16 @@ const user = require("express").Router();
 const uservalidation = require("../middleware/userValidation");
 const userController = require("../controller/UserController");
 
-user.delete("/:id", uservalidation.checkId, userController.remove);
-user.get("/:id", uservalidation.checkId, userController.getById);
+user.delete("/:id",
+  uservalidation.checkId,
+  userController.remove);
+user.get("/:id",
+  uservalidation.checkId,
+  userController.getById);
 user.put(
   "/:id",
   uservalidation.checkId,
+  uservalidation.checkData,
   uservalidation.checkBody,
   userController.update
 );
